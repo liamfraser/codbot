41610d73d3269fd6f756dcf2a3ad19bb5a728c91
Liam Fraser <liam@liamfraser.co.uk>
2014-11-23 19:58:34 +0000

    tidy license

--- codbot.py
+++ codbot.py
@@ -2,7 +2,7 @@
 
 # Copyright (c) 2014, Liam Fraser <liam@liamfraser.co.uk>
 # All rights reserved.
-
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
 #
